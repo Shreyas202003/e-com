c39697c34ec8dcdddeb203c7bed8b7b4aa894b59
Shreyas202003 <shreyassalunkhe2003@gmail.com>
2023-12-20 20:14:16 +0530

    e-com

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>E-Buddy</h1>
+              </div>
+              <p>Happy Shopping</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>Shridevi Institute Of Engineering & Tehnology</li>
+                <li>Email:siet@gmail.com</li>
+                <li>Phone: +91 1123 456 780</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
